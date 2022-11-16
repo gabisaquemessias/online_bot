153531faa497cbade3dc523ffd567a61781d4041
gabisaquemessias <116766510+gabisaquemessias@users.noreply.github.com>
2022-11-16 16:35:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const venom = require('venom-bot');
+
+venom
+    .create({ session: 'ON', multidevice: true })
+    .then((client) => start(client))
+    .catch((erro) => {
+        console.log(erro);
+    });
+
+function start(client) {
+    client.onMessage((message) => {
+        if (message.body !== '') {
+            client
+                .sendContactVcard(message.from, '5511998927372@c.us', 'Vereador Isaque Messias')
+                .then((result) => {
+                })
+                .catch((erro) => {
+                    console.error('Error when sending: ', erro);
+                });
+        }
+    });
+}
